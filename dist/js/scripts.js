b97148cc5dbe086d4374bf1a9d77f3526c420ed3
Nadezhda <nad388@yandex.ru>
2020-12-06 20:24:28 +0300

    the end

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -32,3 +32,40 @@ function toggleSlide(item) {
   };
   toggleSlide('.catalog-item__link');
   toggleSlide('.catalog-item__back');
+
+
+//slider 
+
+function slider() {
+  let slideIndex = 1,
+      slides = document.querySelectorAll('.carousel-item'),
+      prev = document.querySelector('.prev'),
+      next = document.querySelector('.next');
+
+  showSlides(slideIndex);
+
+  function showSlides(n) {
+      if (n > slides.length) {
+          slideIndex = 1;
+      }
+      if (n < 1) {
+          slideIndex = slides.length;
+      }
+      slides.forEach((item) => item.style.display = 'none');
+
+      slides[slideIndex - 1].style.display = 'block';
+
+  }
+  function plusSlides(n) {
+      showSlides(slideIndex += n);
+  }
+ 
+
+  prev.addEventListener('click', function() {
+      plusSlides(-1);
+  });
+  next.addEventListener('click', function() {
+      plusSlides(1);
+  });
+}
+slider();
